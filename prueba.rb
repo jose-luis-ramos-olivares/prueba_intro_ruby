c4a618830dadb7d856874c2417b5f97ad3a7860f
jose <j.ramos.olivares@gmail.com>
2020-07-15 14:19:52 -0400

    method 'build_web_page' added

--- prueba.rb
+++ prueba.rb
@@ -15,3 +15,13 @@ def request(address)
     response = https.request(request)
     JSON.parse response.read_body
 end
+
+def build_web_page(request)
+    pics = request.map{|k, v| k['img_src']}
+    html = "<html>\n<head>\n</head>\n\t<body>\n\t\t<ul>"
+    pics.each do |pic|
+        html += "\n\t\t\t<li><img src='#{pic}'></li>\n"
+    end
+    html += "\t\t</ul>\n\t</body>\n</html>"
+    return html
+end
